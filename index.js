6eafff777818a8bbaa190cc9a967a8cee1c8920d
Jonathan Vega <Jonathan_D_Vega@yahoo.com>
2018-11-26 11:01:51 -0500

    Added jquery for dropdown menu and coor palette

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 
+function watchHover(){
+  $('.js-project-link').hover(event => {
+    $('.js-portfolio-link').addClass('child-hover');
+  });
+
+  $('.js-project-link').mouseleave(event => {
+    $('.js-portfolio-link').removeClass('child-hover');
+  });
+}
 
 function watchSubmit() {
   $('.js-about-link').click(event => 
@@ -109,4 +118,5 @@ function activateDefaultTab(){
 
 
 activateDefaultTab();
+$(watchHover);
 $(watchSubmit);
